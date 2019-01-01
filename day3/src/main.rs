951e36799804ddc021f7b626737d392ae3d191fa
vthib <vthiberville@gmail.com>
2019-01-01 22:49:06 +0100

    day3: add solution for 3-B

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,6 +9,8 @@ use scan_fmt::scan_fmt;
 
 type Result<T> = std::result::Result<T, Box<Error>>;
 
+type Grid = HashMap<(u32, u32), u32>;
+
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
@@ -18,23 +20,33 @@ fn main() -> Result<()> {
         claims.push(line.parse()?);
     }
 
-    println!("day3, part1: {}", part1(&claims));
-    Ok(())
-}
-
-fn part1(claims: &Vec<Claim>) -> usize {
-    let mut grid: HashMap<(u32, u32), u32> = HashMap::new();
-
-    for claim in claims {
+    let mut grid = Grid::new();
+    for claim in &claims {
         for (x,y) in claim.iter_points() {
             *grid.entry((x, y)).or_insert(0) += 1;
         }
     }
+
+    println!("day3, part1: {}", part1(&grid));
+    println!("day3, part2: {}", part2(&claims, &grid)?);
+    Ok(())
+}
+
+fn part1(grid: &Grid) -> usize {
     grid.values().filter(|x| **x > 1).count()
 }
 
+fn part2(claims: &Vec<Claim>, grid: &Grid) -> Result<u32> {
+    for claim in claims {
+        if claim.iter_points().all(|p| grid[&p] == 1) {
+            return Ok(claim.id);
+        }
+    }
+    Err(Box::<Error>::from(format!("no uncontested claims")))
+}
+
 struct Claim {
-    _id: u32,
+    id: u32,
     x: u32,
     y: u32,
     width: u32,
@@ -49,7 +61,7 @@ impl FromStr for Claim {
         let (id, x, y, w, h) = scan_fmt!(s, "#{d} @ {d},{d}: {d}x{d}", u32, u32, u32, u32, u32);
 
         Ok(Claim {
-            _id: id.unwrap(),
+            id: id.unwrap(),
             x: x.unwrap(),
             y: y.unwrap(),
             width: w.unwrap(),
