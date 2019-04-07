e47fa83ff701c18a7416385f048eb45faf4b63d4
vthib <vthiberville@gmail.com>
2019-04-07 17:58:58 +0200

    day8: add solution for 8-A

---
+++ day8/src/main.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::io;
+use std::io::Read;
+use std::iter::Iterator;
+
+type Result<T> = std::result::Result<T, Box<Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let line = input.lines().into_iter().next().unwrap();
+    let mut iter = line.split(' ').map(|s| s.parse::<u32>().unwrap());
+    let root = Node::new(&mut iter);
+
+    println!("day8, part1: total is {}", part1(&root));
+    Ok(())
+}
+
+fn part1(node: &Node) -> u32 {
+    let children_sum: u32 = node.children.iter().map(|n| part1(n)).sum();
+    let node_sum: u32 = node.metadatas.iter().sum();
+
+    children_sum + node_sum
+}
+
+/* {{{ Node */
+
+struct Node {
+    children: Vec<Node>,
+    metadatas: Vec<u32>,
+}
+
+impl Node {
+    fn new<I>(input: &mut I) -> Self
+        where I: Iterator<Item = u32>
+    {
+        let nb_children = input.next().unwrap();
+        let nb_meta = input.next().unwrap();
+
+        let mut children = Vec::with_capacity(nb_children as usize);
+        for _ in 0..nb_children {
+            children.push(Node::new(input));
+        }
+
+        let mut metadatas = Vec::with_capacity(nb_meta as usize);
+        for _ in 0..nb_meta {
+            metadatas.push(input.next().unwrap());
+        }
+
+        Node {
+            children,
+            metadatas,
+        }
+    }
+}
